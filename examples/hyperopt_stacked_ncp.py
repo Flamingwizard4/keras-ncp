bc0f65576d3170b027f3302d33f375c0d29e499d
Flamingwizard4 <jeanmmoorman@gmail.com>
2021-07-30 17:13:04 -0400

    Update hyperopt_stacked_ncp.py

--- examples/hyperopt_stacked_ncp.py
+++ examples/hyperopt_stacked_ncp.py
@@ -16,34 +16,11 @@ from hyperopt.pyll import scope
     (x_valid, y_valid),
 ) = kncp.datasets.icra2020_lidar_collision_avoidance.load_data()
 
-def plot_lidar(lidar, ax):
-    # Helper function for plotting polar-based lidar data
-    angles = np.linspace(-2.35, 2.35, len(lidar))
-    x = lidar * np.cos(angles)
-    y = lidar * np.sin(angles)
-    ax.plot(y, x)
-    ax.scatter([0], [0], marker="^", color="black")
-    ax.set_xlim((-6, 6))
-    ax.set_ylim((-2, 6))
-
 def train_ncp(params):
 
     print("x_train", str(x_train.shape))
     print("y_train", str(y_train.shape))
     
-    # Plot the data
-    sns.set()
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
-    plot_lidar(x_train[0, 0, :, 0], ax1)
-    plot_lidar(x_train[0, 12, :, 0], ax2)
-    plot_lidar(x_train[9, 0, :, 0], ax3)
-    ax1.set_title("Label: {:0.2f}".format(y_train[0, 0, 0]))
-    ax2.set_title("Label: {:0.2f}".format(y_train[0, 12, 0]))
-    ax3.set_title("Label: {:0.2f}".format(y_train[9, 0, 0]))
-    fig.suptitle("LIDAR collision avoidance training examples")
-    fig.savefig("lidar_examples.png")
-
-
     # Build the network
     N = x_train.shape[2]
     channels = x_train.shape[3]
@@ -87,17 +64,6 @@ def train_ncp(params):
 
     model.summary(line_length=100)
 
-    '''# Plot the network architecture
-    sns.set_style("white")
-    plt.figure(figsize=(12, 12))
-    legend_handles = rnn_cell.draw_graph(
-        layout="spiral", neuron_colors={"command": "tab:cyan"}
-    )
-    plt.legend(handles=legend_handles, loc="upper center", bbox_to_anchor=(1, 1))
-    sns.despine(left=True, bottom=True)
-    plt.tight_layout()
-    plt.savefig("architecture.png")'''
-
     # Evaluate the model before training
     print("Validation set MSE before training")
     model.evaluate(x_valid, y_valid)
@@ -130,8 +96,8 @@ def train_ncp(params):
     final_loss = model.evaluate(x_valid, y_valid)
     return final_loss
 
-#HyperOpt
-arch_space = {
+#HyperOpt implementation
+space = {
     'nI': scope.int(hp.quniform('nI', 10, 16, q=1)),
     'nC': scope.int(hp.quniform('nC', 9, 15, q=1)),
     'fS': scope.int(hp.quniform('fS', 4, 10, q=1)), # must be at most the lower bound of nI
@@ -139,7 +105,5 @@ arch_space = {
     'fM': scope.int(hp.quniform('fM', 2, 8, q=1)), # must be at most the lower bound of nC
     'rC': scope.int(hp.quniform('rC', 1, 6, q=1)) # must be at most the lower bound of nC?
 }
-#print(type(3+2+1))
-#print(type('nInter'+'nCommand'+1))
-best_arch = fmin(fn=train_ncp, space=arch_space, algo=tpe.suggest, max_evals=100)
-print(best_arch)
+best = fmin(fn=train_ncp, space=space, algo=tpe.suggest, max_evals=100)
+print(best)
